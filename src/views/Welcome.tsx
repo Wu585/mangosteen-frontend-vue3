571fc2df422f52e903930fdb48a32084fa708201
Wu <15151851516@163.com>
2022-12-09 23:04:34 +0800

    创建欢迎界面相关的组件

---
+++ src/views/Welcome.tsx
@@ -0,0 +1,14 @@
+import {defineComponent} from 'vue';
+import {RouterView} from 'vue-router';
+
+export const Welcome = defineComponent({
+  setup() {
+    return () => (
+      <div>
+        welcome
+        <RouterView/>
+      </div>
+    );
+  }
+});
+
