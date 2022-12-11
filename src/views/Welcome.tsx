bbe1599f2e0ace3cb387c3cdafeb66c8a2e2b1d2
Wu <15151851516@163.com>
2022-12-11 19:37:50 +0800

    完成 useSwipe

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,18 +1,24 @@
-import {defineComponent, Transition, VNode} from 'vue';
+import {defineComponent, ref, Transition, VNode, watchEffect} from 'vue';
 import {RouterView} from 'vue-router';
 import s from './Welcome.module.scss';
+import {useSwipe} from '../hooks/useSwipe';
 
 export const Welcome = defineComponent({
   setup() {
+    const main = ref<HTMLElement | null>(null);
+    const {direction} = useSwipe(main);
+    watchEffect(() => {
+      console.log(direction.value);
+    });
     return () => (
       <div class={s.wrapper}>
         <header>
           <svg>
-            <use xlinkHref='#mangosteen'></use>
+            <use xlinkHref="#mangosteen"></use>
           </svg>
           <h1>山竹记账</h1>
         </header>
-        <main>
+        <main ref={main}>
           <RouterView name="main">
             {({Component: Content}: { Component: VNode }) =>
               <Transition enterFromClass={s.slide_fade_enter_from} enterActiveClass={s.slide_fade_enter_active}
