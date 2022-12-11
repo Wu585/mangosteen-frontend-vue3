412bc6b569d8372470470d2966b6a5575516a90e
Wu <15151851516@163.com>
2022-12-11 18:26:30 +0800

    完成 vite svgstore plugin

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,14 +1,15 @@
 import {defineComponent, Transition, VNode} from 'vue';
 import {RouterView} from 'vue-router';
 import s from './Welcome.module.scss';
-import logo from '../assets/icons/mangosteen.svg';
 
 export const Welcome = defineComponent({
   setup() {
     return () => (
       <div class={s.wrapper}>
         <header>
-          <img src={logo} alt=""/>
+          <svg>
+            <use xlinkHref='#mangosteen'></use>
+          </svg>
           <h1>山竹记账</h1>
         </header>
         <main>
