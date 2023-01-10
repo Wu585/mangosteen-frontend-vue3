342078e312686f4bf9dadea814f9a38ce82e3613
Wu <15151851516@163.com>
2023-01-10 16:56:14 +0800

    制作 Center 组件

---
+++ src/components/center/Center.tsx
@@ -0,0 +1,20 @@
+import {defineComponent, PropType} from 'vue';
+import s from './Center.module.scss';
+
+export const Center = defineComponent({
+  props: {
+    direction: {
+      type: String as PropType<'-' | '|' | 'horizontal' | 'vertical'>,
+      default: 'horizontal'
+    }
+  },
+  setup(props, context) {
+    const extraClass = props.direction === '-' || props.direction === 'horizontal' ? 'horizontal' : 'vertical';
+    return () => (
+      <div class={[s.center, extraClass]}>
+        {context.slots.default?.()}
+      </div>
+    );
+  }
+});
+
--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -2,11 +2,17 @@ import {defineComponent} from 'vue';
 import {Button} from '../components/button/Button';
 import s from './StartPage.module.scss';
 import {FloatButton} from '../components/floatbutton/FloatButton';
+import {Center} from '../components/center/Center';
+import {Icon} from '../components/icon/Icon';
 
 export const StartPage = defineComponent({
   setup() {
     return () => (
-      <div>
+      <div class={s.start_page}>
+        <nav>nav</nav>
+        <Center class={s.pig_wrapper}>
+          <Icon name="pig" class={s.pig}/>
+        </Center>
         <div class={s.button_wrapper}>
           <Button class={s.button}>button</Button>
         </div>
