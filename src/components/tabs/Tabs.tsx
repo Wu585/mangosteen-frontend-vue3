c6816466706576c9f46b600ab3a3a09f0f6827b8
Wu <15151851516@163.com>
2023-01-12 14:56:21 +0800

    完成 Tabs 组件

--- src/components/tabs/Tabs.tsx
+++ src/components/tabs/Tabs.tsx
@@ -14,21 +14,24 @@ export const Tabs = defineComponent({
   },
   setup(props, {slots}) {
     return () => {
-      const array = slots.default?.();
-      if (!array) return null;
-      for (let i = 0; i < array.length; i++) {
-        if (array[i].type !== Tab) {
+      const tabs = slots.default?.();
+      if (!tabs) return null;
+      for (let i = 0; i < tabs.length; i++) {
+        if (tabs[i].type !== Tab) {
           throw new Error('<Tabs> only accepts <Tab> as children');
         }
       }
       return <div class={s.tabs}>
         <ol class={s.tabs_nav}>
-          {array.map(item => <li
+          {tabs.map(item => <li
             onClick={() => props.onUpdateSelected?.(item.props?.name)}
             class={props.selected === item.props?.name ? s.selected : ''}>
             {item.props?.name}
           </li>)}
         </ol>
+        <div>
+          {tabs.find(item => item.props?.name === props.selected)}
+        </div>
       </div>;
     };
   }
