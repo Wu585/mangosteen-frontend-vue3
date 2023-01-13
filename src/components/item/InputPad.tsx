8f4141abe1d856741f5bae8e908051a4ab7d456c
Wu <15151851516@163.com>
2023-01-13 10:57:25 +0800

    完成数字键盘的样式

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -6,60 +6,48 @@ import {time} from '../../utils/time';
 
 export const InputPad = defineComponent({
   setup() {
+    const refAmount = ref('0')
+    const appendText = (n: number | string) => {
+      const nString = n.toString()
+      const dotIndex = refAmount.value.indexOf('.')
+      if (refAmount.value.length >= 13) {
+        return
+      }
+      if (dotIndex >= 0 && refAmount.value.length - dotIndex > 2) {
+        return
+      }
+      if (nString === '.') {
+        if (dotIndex >= 0) { // 已经有小数点了
+          return
+        }
+      } else if (nString === '0') {
+        if (dotIndex === -1) { // 没有小数点
+          if (refAmount.value === '0') { // 没小数点，但是有0
+            return
+          }
+        }
+      } else {
+        if (refAmount.value === '0') {
+          refAmount.value = ''
+        }
+      }
+      refAmount.value += n.toString()
+    }
     const buttons = [
-      {
-        text: '1', onClick: () => {
-        }
-      },
-      {
-        text: '2', onClick: () => {
-        }
-      },
-      {
-        text: '3', onClick: () => {
-        }
-      },
-      {
-        text: '4', onClick: () => {
-        }
-      },
-      {
-        text: '5', onClick: () => {
-        }
-      },
-      {
-        text: '6', onClick: () => {
-        }
-      },
-      {
-        text: '7', onClick: () => {
-        }
-      },
-      {
-        text: '8', onClick: () => {
-        }
-      },
-      {
-        text: '9', onClick: () => {
-        }
-      },
-      {
-        text: '.', onClick: () => {
-        }
-      },
-      {
-        text: '0', onClick: () => {
-        }
-      },
-      {
-        text: '清空', onClick: () => {
-        }
-      },
-      {
-        text: '提交', onClick: () => {
-        }
-      },
-    ];
+      { text: '1', onClick: () => { appendText(1) } },
+      { text: '2', onClick: () => { appendText(2) } },
+      { text: '3', onClick: () => { appendText(3) } },
+      { text: '4', onClick: () => { appendText(4) } },
+      { text: '5', onClick: () => { appendText(5) } },
+      { text: '6', onClick: () => { appendText(6) } },
+      { text: '7', onClick: () => { appendText(7) } },
+      { text: '8', onClick: () => { appendText(8) } },
+      { text: '9', onClick: () => { appendText(9) } },
+      { text: '.', onClick: () => { appendText('.') } },
+      { text: '0', onClick: () => { appendText(0) } },
+      { text: '清空', onClick: () => { refAmount.value = '0' } },
+      { text: '提交', onClick: () => { } },
+    ]
     const popUpVisibleRef = ref(false);
     const showPopUp = () => popUpVisibleRef.value = true;
     const hidePopUp = () => popUpVisibleRef.value = false;
