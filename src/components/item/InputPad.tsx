06d614558453f68745560f51af51ee03fdfa7079
Wu <15151851516@163.com>
2023-01-13 10:27:27 +0800

    引入 DatePicker 组件

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -1,35 +1,91 @@
-import {defineComponent} from 'vue';
+import {defineComponent, ref} from 'vue';
 import s from './InputPad.module.scss';
 import {Icon} from '../icon/Icon';
+import {DatePicker, Popup} from 'vant';
+import {time} from '../../utils/time';
 
 export const InputPad = defineComponent({
   setup() {
     const buttons = [
-      { text: '1', onClick: () => {  } },
-      { text: '2', onClick: () => { } },
-      { text: '3', onClick: () => {  } },
-      { text: '4', onClick: () => {  } },
-      { text: '5', onClick: () => {  } },
-      { text: '6', onClick: () => {  } },
-      { text: '7', onClick: () => {  } },
-      { text: '8', onClick: () => {  } },
-      { text: '9', onClick: () => {} },
-      { text: '.', onClick: () => {  } },
-      { text: '0', onClick: () => {  } },
-      { text: '清空', onClick: () => {} },
-      { text: '提交', onClick: () => { } },
-    ]
+      {
+        text: '1', onClick: () => {
+        }
+      },
+      {
+        text: '2', onClick: () => {
+        }
+      },
+      {
+        text: '3', onClick: () => {
+        }
+      },
+      {
+        text: '4', onClick: () => {
+        }
+      },
+      {
+        text: '5', onClick: () => {
+        }
+      },
+      {
+        text: '6', onClick: () => {
+        }
+      },
+      {
+        text: '7', onClick: () => {
+        }
+      },
+      {
+        text: '8', onClick: () => {
+        }
+      },
+      {
+        text: '9', onClick: () => {
+        }
+      },
+      {
+        text: '.', onClick: () => {
+        }
+      },
+      {
+        text: '0', onClick: () => {
+        }
+      },
+      {
+        text: '清空', onClick: () => {
+        }
+      },
+      {
+        text: '提交', onClick: () => {
+        }
+      },
+    ];
+    const popUpVisibleRef = ref(false);
+    const showPopUp = () => popUpVisibleRef.value = true;
+    const hidePopUp = () => popUpVisibleRef.value = false;
+    const now = new Date();
+    const dateRef = ref<Date>(now);
+    const currentDateRef = ref(time(dateRef.value).format().split('-'));
+    const setDate = (date: any) => {
+      currentDateRef.value = date.selectedValues;
+      hidePopUp();
+    };
     return () => (
       <div class={s.wrapper}>
         <div class={s.dateAndAmount}>
           <span class={s.date}>
             <Icon name={'date'} class={s.icon}/>
-            <span>2022-01-01</span>
+            <span onClick={showPopUp}>{currentDateRef.value.join('-')}</span>
+            <Popup position={'bottom'} v-model:show={popUpVisibleRef.value}>
+              <DatePicker value={currentDateRef.value}
+                          title="选择日期" onCancel={hidePopUp}
+                          onConfirm={setDate}/>
+            </Popup>
           </span>
           <span class={s.amount}>199.2</span>
         </div>
         <div class={s.buttons}>
-          {buttons.map(item=> <button onClick={item.onClick}>{item.text}</button>)}
+          {buttons.map(item => <button onClick={item.onClick}>{item.text}</button>)}
         </div>
       </div>
     );
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import {createApp} from 'vue';
 import {App} from './App';
 import {router} from './router';
 import "@svgstore"
+import 'vant/lib/index.css';
 
 const app = createApp(App);
 
