feb178303260dd49a20ccc97dc1f9a774ef0d47b
Wu <15151851516@163.com>
2023-01-13 13:54:56 +0800

    创建 Tags 相关组件

---
+++ src/components/tag/TagEdit.tsx
@@ -0,0 +1,12 @@
+import {defineComponent} from 'vue';
+
+export const TagEdit = defineComponent({
+  setup() {
+    return () => (
+      <div>
+        TagEdit
+      </div>
+    );
+  }
+});
+
---
+++ src/views/TagPage.tsx
@@ -0,0 +1,13 @@
+import {defineComponent} from 'vue';
+import {RouterView} from 'vue-router';
+
+export const TagPage = defineComponent({
+  setup() {
+    return () => (
+      <div>
+        <RouterView/>
+      </div>
+    );
+  }
+});
+
