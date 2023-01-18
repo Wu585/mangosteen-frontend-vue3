a2d6bd7f113b5b4e9493ffe35e20cf0cfbeeb6a4
Wu <15151851516@163.com>
2023-01-18 15:10:01 +0800

    完成三个图表

--- src/components/charts/Charts.tsx
+++ src/components/charts/Charts.tsx
@@ -2,7 +2,9 @@ import {defineComponent, onMounted, ref} from 'vue';
 import {Form} from '../form/Form';
 import {FormItem} from '../form/FormItem';
 import s from './Charts.module.scss';
-import * as echarts from 'echarts';
+import {LineChart} from './LineChart';
+import {PieCharts} from './PieCharts';
+import {Bars} from './Bars';
 
 export const Charts = defineComponent({
   props: {
@@ -17,30 +19,7 @@ export const Charts = defineComponent({
   },
   setup() {
     const category = ref('expense');
-    const refDiv = ref<HTMLElement>();
-    onMounted(() => {
-      if (refDiv.value === undefined) return;
-      // 基于准备好的dom，初始化echarts实例
-      const myChart = echarts.init(refDiv.value);
-      // 绘制图表
-      const option = {
-        grid: [{left: 0, top: 0, right: 0, bottom: 20}],
-        xAxis: {
-          type: 'category',
-          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-        },
-        yAxis: {
-          type: 'value'
-        },
-        series: [
-          {
-            data: [150, 230, 224, 218, 135, 147, 260],
-            type: 'line'
-          }
-        ]
-      };
-      myChart.setOption(option);
-    });
+
     return () => (
       <div class={s.wrapper}>
         <Form>
@@ -49,7 +28,9 @@ export const Charts = defineComponent({
             {value: 'income', text: '收入'}
           ]} v-model={category.value}/>
         </Form>
-        <div ref={refDiv} class={s.demo}></div>
+        <LineChart/>
+        <PieCharts/>
+        <Bars/>
       </div>
     );
   }
