a089ba697f176da1c0ffcc13ffd6235a016c6c3f
Wu <15151851516@163.com>
2023-01-18 14:43:56 +0800

    echarts 实现折线图

--- src/components/charts/Charts.tsx
+++ src/components/charts/Charts.tsx
@@ -1,7 +1,8 @@
-import {defineComponent, ref} from 'vue';
+import {defineComponent, onMounted, ref} from 'vue';
 import {Form} from '../form/Form';
 import {FormItem} from '../form/FormItem';
-import s from './Charts.module.scss'
+import s from './Charts.module.scss';
+import * as echarts from 'echarts';
 
 export const Charts = defineComponent({
   props: {
@@ -15,15 +16,40 @@ export const Charts = defineComponent({
     }
   },
   setup() {
-    const category = ref('expense')
+    const category = ref('expense');
+    const refDiv = ref<HTMLElement>();
+    onMounted(() => {
+      if (refDiv.value === undefined) return;
+      // 基于准备好的dom，初始化echarts实例
+      const myChart = echarts.init(refDiv.value);
+      // 绘制图表
+      const option = {
+        grid: [{left: 0, top: 0, right: 0, bottom: 20}],
+        xAxis: {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            data: [150, 230, 224, 218, 135, 147, 260],
+            type: 'line'
+          }
+        ]
+      };
+      myChart.setOption(option);
+    });
     return () => (
       <div class={s.wrapper}>
         <Form>
           <FormItem label={'类型'} type={'select'} options={[
-            {value:'expense',text:'支出'},
-            {value:'income',text:'收入'}
+            {value: 'expense', text: '支出'},
+            {value: 'income', text: '收入'}
           ]} v-model={category.value}/>
         </Form>
+        <div ref={refDiv} class={s.demo}></div>
       </div>
     );
   }
