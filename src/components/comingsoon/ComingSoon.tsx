92eb65e256241332c820aabf96867c44b466fd61
Wu <15151851516@163.com>
2023-02-13 17:42:14 +0800

    添加敬请期待

---
+++ src/components/comingsoon/ComingSoon.tsx
@@ -0,0 +1,22 @@
+import {defineComponent, PropType} from 'vue';
+import s from './ComingSoon.module.scss';
+import {Center} from '../center/Center';
+import {Icon} from '../icon/Icon';
+
+export const ComingSoon = defineComponent({
+  props: {
+    name: {
+      type: String as PropType<string>
+    }
+  },
+  setup: () => {
+    return () => (
+      <div>
+        <Center class={s.pig_wrapper}>
+          <Icon name="pig" class={s.pig}/>
+        </Center>
+        <p class={s.text}>敬请期待</p>
+      </div>
+    );
+  }
+});
