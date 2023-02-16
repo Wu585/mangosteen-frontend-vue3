0b106d940cb62f8f9d3b7917239ecacf6835cba8
Wu <15151851516@163.com>
2023-02-16 20:36:06 +0800

    解决 onMounted 中未处理的 error 警告

--- src/hooks/useAfterMe.ts
+++ src/hooks/useAfterMe.ts
@@ -3,7 +3,27 @@ import {mePromise} from '../utils/me';
 
 export const useAfterMe = (fn: () => void) => {
   onMounted(async () => {
-    await mePromise;
+    try {
+      await mePromise;
+    } catch (err) {
+      return;
+    }
+    /*// return 一个error相当于在这里把错误处理了，后续代码还会执行
+    await mePromise?.catch(err => {
+      return err;
+    });
+    // 这里如果throw一个error，那么控制台还会有未处理的报错
+    await mePromise?.catch(err => {
+      throw err;
+    });
+    // 正确做法
+    const result = await mePromise?.catch(error => new Error());
+    console.log(result);
+    if (result instanceof Error) {
+      return;
+    }
+    // 也可以直接把后续放在then里，但第二个参数还是要处理一下的
+    mePromise?.then(fn, () => undefined);*/
     fn();
   });
 };
